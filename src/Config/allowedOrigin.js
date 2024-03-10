3413ff90f80686d8f891c87d0a8196f583e42953
Ankesh Kumar <60851047+notelyfe@users.noreply.github.com>
2024-03-10 16:08:24 +0530

    Update allowedOrigin.js

--- src/Config/allowedOrigin.js
+++ src/Config/allowedOrigin.js
@@ -1,5 +1,5 @@
 // const allowedOrigins = ["https://docs.notelyfe.me"]
-const allowedOrigins = ["https://docs.notelyfe.me"]
+const allowedOrigins = ["https://docs.notelyfe.me", "https://notelyfe.me"]
 // const allowedOrigins = ["http://localhost:3000"]
 
 module.exports = allowedOrigins
